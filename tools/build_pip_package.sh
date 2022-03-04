70d9c83c4b74f713a2339df7b46a9c6c03be8201
JC Louis <jean-charles.louis@lifen.fr>
2022-03-04 15:59:06 +0100

    add python3.10

--- tools/build_pip_package.sh
+++ tools/build_pip_package.sh
@@ -232,11 +232,13 @@ elif [ ${ARG} == "ALL_VERSIONS" ]; then
   # Compile with all the version of python using pyenv.
   assemble_files
   eval "$(pyenv init -)"
+  e2e_pyenv 3.10.2
   e2e_pyenv 3.9.2
   e2e_pyenv 3.8.7
   e2e_pyenv 3.7.7
 elif [ ${ARG} == "ALL_VERSIONS_ALREADY_ASSEMBLED" ]; then
   eval "$(pyenv init -)"
+  e2e_pyenv 3.10.2
   e2e_pyenv 3.9.2
   e2e_pyenv 3.8.7
   e2e_pyenv 3.7.7
