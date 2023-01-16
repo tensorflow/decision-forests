031d74ae0f70b82096774c28fa4bd72d2e87ce81
Guillermo Etchebarne <getchebarne@tryolabs.com>
2023-01-16 12:35:11 -0300

    Implementing assign operator

--- tensorflow_decision_forests/contrib/temporal_feature_processor/temporal_feature_processor/implementation/pandas/operators/assign.py
+++ tensorflow_decision_forests/contrib/temporal_feature_processor/temporal_feature_processor/implementation/pandas/operators/assign.py
@@ -5,7 +5,7 @@ from .base import PandasOperator
 
 class PandasAssignOperator(PandasOperator):
 
-  def __call__(self, event: PandasEvent, features: PandasEvent) -> PandasEvent:
+  def __call__(self, event_1: PandasEvent, event_2: PandasEvent) -> PandasEvent:
     """Assign features to an event.
         Input event and features must have same index.
         Features cannot have more than one row for a single index + timestamp occurence.
@@ -19,5 +19,24 @@ class PandasAssignOperator(PandasOperator):
         Returns:
             PandasEvent: a new event with the features assigned.
         """
-    # TODO: implement logic.
-    pass
+    # assert indexes are the same
+    if event_1.index.names != event_2.index.names:
+      raise IndexError("Assign sequences must have the same index names.")
+
+    # get index column names
+    index, timestamp = self.split_index(event_1)
+
+    # check there's no repeated timestamps index-wise in the assigned sequence
+    if index:
+      max_timestamps = (
+          event_2.reset_index().groupby(index)[timestamp].value_counts().max())
+    else:
+      max_timestamps = event_2.reset_index()[timestamp].value_counts().max()
+
+    if max_timestamps > 1:
+      raise ValueError(
+          "Cannot have repeated timestamps in assigned EventSequence.")
+
+    # make assignment
+    output = event_1.join(event_2, how="left", rsuffix="y")
+    return output
