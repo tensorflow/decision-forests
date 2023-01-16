c6b2e48aa73ebb011878efe38f44bf53229f4bb2
Guillermo Etchebarne <getchebarne@tryolabs.com>
2023-01-16 12:58:19 -0300

    Adding split_index method to PandasOperator class

--- tensorflow_decision_forests/contrib/temporal_feature_processor/temporal_feature_processor/implementation/pandas/operators/base.py
+++ tensorflow_decision_forests/contrib/temporal_feature_processor/temporal_feature_processor/implementation/pandas/operators/base.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import Any
+from typing import Any, List, Tuple
 
 from temporal_feature_processor.implementation.pandas.data.event import PandasEvent
 
@@ -14,3 +14,17 @@ class PandasOperator(ABC):
         Returns:
             PandasEvent: the output event of the operator.
         """
+
+  def split_index(self, event: PandasEvent) -> Tuple[List[str], str]:
+    """Split pandas' DataFrame index into (potentially) index columns and
+        a timestamp column.
+        Args:
+            event (PandasEvent): input PandasEvent (pandas DataFrame).
+        Returns:
+            Tuple[List[str], str]: output index and timestamp names.
+        """
+    index_timestamp = event.index.names
+    index = index_timestamp[:-1]
+    timestamp = index_timestamp[-1]
+
+    return index, timestamp
